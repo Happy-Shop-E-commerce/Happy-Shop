2c0f064680846faa9a57b4d035e06b9afc2f73e5
unknown <ilyessdridi37@gmail.com>
2022-12-13 17:59:34 +0100

    auth done

--- Server/database/index.js
+++ Server/database/index.js
@@ -1,6 +1,6 @@
 var mongoose = require("mongoose");
-var mongoUri = "";
-
+var mongoUri = "mongodb+srv://happy:shop@cluster0.tq8hta3.mongodb.net/?retryWrites=true&w=majority";
+mongoose.set('strictQuery', false);
 mongoose.connect(mongoUri, () => {
   console.log("Database connected  :)");
 });
--- Server/index.js
+++ Server/index.js
@@ -1,13 +1,21 @@
 var express = require("express");
-var morgan = require("morgan");
+var cors=require("cors")
 
 var app = express();
-app.use(morgan("dev"));
+app.use(cors())
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(__dirname + '../client/public'));
+
+
+app.get('/', function (req, res) {
+  res.status(200).json({ message: 'Welcome to the car RESTful API!' });
+}); 
+app.use("/users", require('./user/userRoutes'))
+
 
 var port = 4000;
 
 app.listen(port, () => {
-  console.log("yup connected : " + port);
+  console.log("server connected : " + port);
 });
