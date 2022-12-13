466f03aa282253705331c60529966c11d0b971e1
unknown <ilyessdridi37@gmail.com>
2022-12-13 22:59:16 +0100

    auth for admin done !!

--- Server/Product/ProductRouter.js
+++ Server/Product/ProductRouter.js
@@ -1,12 +1,14 @@
 const router = require("express").Router();
-const post = require('./ProductSchema.js')
+const post = require('./ProductSchema.js') 
+const auth = require("../middleware/auth") 
+const admin = require("../middleware/admin")
   const {getAll,getOne,deleteOne,createOne,updateOne }= require('./ProductController')
 
    router.get("/",getAll)
     router.get("/:id",getOne)
 
-    router.delete("/:id",deleteOne)
-    router.put("/:id",updateOne)
-    router.post("/",createOne)
+    router.delete("/:id",[auth,admin],deleteOne)
+    router.put("/:id",[auth,admin],updateOne)
+    router.post("/",[auth,admin],createOne)
 
     module.exports = router;
